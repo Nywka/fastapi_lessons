e7bfdfee8cf8738a552d37ebe361ad2bcdbad93e
Borisov Aleksandr <aleksborisov107@gmail.com>
2025-04-05 15:03:42 +0300

    добавлена работа с бд

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 import uvicorn
 
+from core.models import Base, db_helper
 from items_views import router as items_router
 from Users.views import router as users_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(items_router)
 app.include_router(users_router)
 
+
 if __name__ == "__main__":
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run("main:app", reload=True)
